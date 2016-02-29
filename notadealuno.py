c7738fec5a88a8584106a45b27db9bd71930911b
felipeclaudino <claudino_67@hotmail.com>
2016-02-29 17:32:04 -0300

    Delete notadealuno.py

--- notadealuno.py
+++
@@ -1,8 +0,0 @@
-nome = input("insira o seu nome: ")
-a = float (input("insira sua 1º nota: ")
-b = float (input("insira sua 2º nota: ")
-media = (a + b) / 2
-if  media >=7:
-	print "aprovado"
-else: 
-	print "reprovado"
