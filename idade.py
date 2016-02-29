1afb4abc3c1c5fbae7e4e0f78b80dfd7067619ec
felipeclaudino <claudino_67@hotmail.com>
2016-02-29 17:32:47 -0300

    Delete idade.py

--- idade.py
+++
@@ -1,7 +0,0 @@
-idade= int(input("insira sua idade: "))
-maior= ("você é maior de idade")
-menor= ("você é menor de idade")
-if idade >= 18:
-	print("maior de idade acesso liberado.")
-else:
-	print("menor de idade acesso negado.")
